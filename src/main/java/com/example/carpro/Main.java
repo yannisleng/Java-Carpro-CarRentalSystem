59bffd32b70f86b2fafb3673fa6d51e9f3548b26
pheymin <pheymin1223@gmail.com>
2022-11-28 13:07:00 +0800

    update admin profile

--- src/main/java/com/example/carpro/Main.java
+++ src/main/java/com/example/carpro/Main.java
@@ -19,8 +19,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        //FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("loginPage.fxml"));
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("adminMain.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("loginPage.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load(), 900, 600);
         stage.setScene(scene);
